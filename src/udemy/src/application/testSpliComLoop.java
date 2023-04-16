22127e199a50e5e5592000edfa42505f959bc9b3
Matsz <matzmsn@gmail.com>
2023-04-16 13:41:25 -0300

    exJava

---
+++ src/udemy/src/application/testSpliComLoop.java
@@ -0,0 +1,35 @@
+package udemy.src.application;
+
+public class testSpliComLoop {
+    public static void main(String[] args) {
+
+        String a = "potato ice cube bala apple orange";
+
+        String[] vect = a.split(" ");
+
+        if (vect.length >= 6) {
+            String word1 = vect[0];
+            String word2 = vect[1];
+            String word3 = vect[2];
+            String word4 = vect[3];
+            String word5 = vect[4];
+            String word6 = vect[5];
+
+            System.out.println(word1);
+            System.out.println(word2);
+            System.out.println(word3);
+            System.out.println(word4);
+            System.out.println(word5);
+            System.out.println(word6);
+        }
+        else {
+        System.out.println("O array não tem pelo menos 6 elementos.");
+        }
+
+        // Usando loop para percorrer o array
+        for (String word : vect) {
+            System.out.println(word);
+        }
+    }
+}
+
---
+++ src/udemy/src/application/testSplit.java
@@ -0,0 +1,33 @@
+package udemy.src.application;
+
+public class testSplit {
+    public static void main(String[] args) {
+
+        String a = "potato ice limão cube apple orange";
+
+        String[] vect = a.split(" ");
+
+
+            String word1 = vect[0];
+            String word2 = vect[1];
+            String word3 = vect[2];
+            String word4 = vect[3];
+            String word5 = vect[4];
+            String word6 = vect[5];
+
+            System.out.println(word1);
+            System.out.println(word2);
+            System.out.println(word3);
+            System.out.println(word4);
+            System.out.println(word5);
+            System.out.println(word6);
+
+        }
+
+
+
+
+
+        }
+
+
