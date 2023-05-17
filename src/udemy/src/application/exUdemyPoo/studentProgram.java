953bcb19539ef1d5cbf332bf44e19f4b4409a94d
Matsz <matzmsn@gmail.com>
2023-05-17 14:19:54 -0300

    exercicio

---
+++ src/udemy/src/application/exUdemyPoo/studentProgram.java
@@ -0,0 +1,34 @@
+package udemy.src.application.exUdemyPoo;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class studentProgram {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Student student = new Student();
+
+        System.out.print("Name: ");
+        student.name = sc.nextLine();
+        System.out.print("Grade 1: ");
+        student.grade1 = sc.nextDouble();
+        System.out.print("Grade 2: ");
+        student.grade2 = sc.nextDouble();
+        System.out.print("Grade 3: ");
+        student.grade3 = sc.nextDouble();
+
+        System.out.printf("Final grade: %.2f%n", student.finalGrade());
+
+        if (student.finalGrade() < 60.0) {
+            System.out.println("Failed");
+            System.out.printf("Missing %.2f points%n", student.missingPoints());
+        }
+        else {
+            System.out.println("Pass");
+        }
+
+        sc.close();
+    }
+}
