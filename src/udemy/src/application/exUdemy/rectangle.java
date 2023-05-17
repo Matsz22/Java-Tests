1e0a6b788b3b5ac8684477d62589bf45f7c98bc3
Matsz <matzmsn@gmail.com>
2023-05-17 12:02:49 -0300

    exercicio

---
+++ src/udemy/src/application/exUdemy/rectangle.java
@@ -0,0 +1,17 @@
+package udemy.src.application.exUdemy;
+
+public class rectangle {
+
+    public double width;
+    public double height;
+
+    public double area() {
+        return width * height;
+    }
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+    public double diagonal() {
+        return Math.sqrt(width * height + width * height);
+    }
+}
