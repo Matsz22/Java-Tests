cd049807e95bf93af4958d1f856206719c4ed2ea
Matsz <matzmsn@gmail.com>
2023-05-18 17:50:47 -0300

    ex Raio com membros estaticos

--- src/udemy/src/application/exMembrosEst/program.java
+++ src/udemy/src/application/exMembrosEst/program.java
@@ -7,18 +7,17 @@ public class program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        calculator calc = new calculator();
 
         System.out.print("Enter radius: ");
         double radius = sc.nextDouble();
 
-        double c = calc.circumference(radius);
+        double c = calculator.circumference(radius);
 
-        double v = calc.volume(radius);
+        double v = calculator.volume(radius);
 
         System.out.printf("Circumference: %.2f%n", c);
         System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("PI value: %.2f%n", calc.PI);
+        System.out.printf("PI value: %.2f%n", calculator.PI);
 
         sc.close();
     }
