d7ca0b8afbc6341b2581bb05c7708b56f55a27cb
Matsz <matzmsn@gmail.com>
2023-05-18 17:44:56 -0300

    ex Raio com membros de instancia sem static

--- src/udemy/src/application/exMembrosEst/calculator.java
+++ src/udemy/src/application/exMembrosEst/calculator.java
@@ -1,11 +1,11 @@
 package udemy.src.application.exMembrosEst;
 
 public class calculator {
-    public static final double PI = 3.14159;
-    public static double circumference(double radius) {
+    public final double PI = 3.14159;
+    public double circumference(double radius) {
         return 2.0 * PI * radius;
     }
-    public static double volume(double radius) {
+    public double volume(double radius) {
         return 4.0 * PI * radius * radius * radius /3;
     }
 }
